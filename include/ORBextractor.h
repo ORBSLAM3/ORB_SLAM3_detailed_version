97590d52ba96c6f370514ad5f891ea5348d0d8b9
qxiaofan <qiyong1993@163.com>
2020-12-08 20:30:23 +0800

    comment ORBextractor.h

--- include/ORBextractor.h
+++ include/ORBextractor.h
@@ -64,6 +64,15 @@ public:
     
     enum {HARRIS_SCORE=0, FAST_SCORE=1 };
 
+   /**
+   * @brief 构造函数
+   *
+   * @param[in] nfeatures   特征点数目
+   * @param[in] scaleFactor 图像金字塔缩放系数
+   * @param[in] nlevels     金字塔层数
+   * @param[in] iniThFAST   最初FAST特征点提取参数
+   * @param[in] minThFAST   使用iniThFAST阈值，提取的特征点数目达不到要求时，启用阈值minThFAST
+   */
     ORBextractor(int nfeatures, float scaleFactor, int nlevels,
                  int iniThFAST, int minThFAST);
 
@@ -101,29 +110,53 @@ public:
     std::vector<cv::Mat> mvImagePyramid;
 
 protected:
-
+    /**
+    * @brief 计算图像金字塔
+    * @param[in] image 输入图片
+    */
     void ComputePyramid(cv::Mat image);
-    void ComputeKeyPointsOctTree(std::vector<std::vector<cv::KeyPoint> >& allKeypoints);    
+
+   /**
+   * @brief 按八叉树分配特征点方式，计算图像金字塔中的特征点
+   * @details 第一层vector：存储image的所有特征点；第二层vector:每层金字塔对应的keypoints
+   * @param[out] allKeypoints 提取得到的所有特征点
+   */
+    void ComputeKeyPointsOctTree(std::vector<std::vector<cv::KeyPoint> >& allKeypoints);
+
+   /**
+   * @brief 对于某一图层，分配其特征点，通过八叉树的方式
+   * @param[in] vToDistributeKeys         等待分配的特征点
+   * @param[in] minX                      分发的图像范围
+   * @param[in] maxX                      分发的图像范围
+   * @param[in] minY                      分发的图像范围
+   * @param[in] maxY                      分发的图像范围
+   * @param[in] nFeatures                 设定的、本图层中想要提取的特征点数目
+   * @param[in] level                     要提取的图像所在的金字塔层
+   * @return std::vector<cv::KeyPoint>
+   */
     std::vector<cv::KeyPoint> DistributeOctTree(const std::vector<cv::KeyPoint>& vToDistributeKeys, const int &minX,
                                            const int &maxX, const int &minY, const int &maxY, const int &nFeatures, const int &level);
-
+   /**
+   * @brief 这个函数实际中没有使用，不作介绍
+   * @param[out] allKeypoints 提取到的特征点
+   */
     void ComputeKeyPointsOld(std::vector<std::vector<cv::KeyPoint> >& allKeypoints);
-    std::vector<cv::Point> pattern;
+    std::vector<cv::Point> pattern;      //计算描述子的随机采样点
 
-    int nfeatures;
-    double scaleFactor;
-    int nlevels;
-    int iniThFAST;
-    int minThFAST;
+    int nfeatures;                       //整个图像(囊括每层金字塔）应提取的特征点数目
+    double scaleFactor;                  //图像金字塔缩放因子
+    int nlevels;                         //图像金字塔层数
+    int iniThFAST;                       //初始FAST响应阈值
+    int minThFAST;                       //最小FAST响应阈值
 
-    std::vector<int> mnFeaturesPerLevel;
+    std::vector<int> mnFeaturesPerLevel; //每层图像中该提取的特征点数目
 
     std::vector<int> umax;
 
-    std::vector<float> mvScaleFactor;
-    std::vector<float> mvInvScaleFactor;    
-    std::vector<float> mvLevelSigma2;
-    std::vector<float> mvInvLevelSigma2;
+    std::vector<float> mvScaleFactor;    //每层缩放比例
+    std::vector<float> mvInvScaleFactor; //每层缩放比例的倒数
+    std::vector<float> mvLevelSigma2;    //每层的sigma^2
+    std::vector<float> mvInvLevelSigma2; //1/(sigma^2)
 };
 
 } //namespace ORB_SLAM
